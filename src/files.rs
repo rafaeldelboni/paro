0e090e1ce86cdab7fe4887618e6f16eaf5ddc48f
Rafael Delboni <rafadelboni@gmail.com>
2022-08-16 09:45:06 -0300

    test: Adds integration test for walkdir

--- src/files.rs
+++ src/files.rs
@@ -12,3 +12,33 @@ pub fn walk_directories(directories: Vec<String>) -> Vec<DirEntry> {
   }
   paths
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_walk_directories() {
+    let files = walk_directories(vec![
+      "tests/example-dotfiles/folder".to_string(),
+      "tests/example-dotfiles/tag-um".to_string(),
+    ]);
+    let str_files: Vec<String> = files
+      .clone()
+      .into_iter()
+      .map(|e| e.path().to_str().unwrap().to_string())
+      .collect();
+
+    assert_eq!(str_files.len(), 5);
+    assert_eq!(
+      str_files,
+      vec![
+        "tests/example-dotfiles/folder",
+        "tests/example-dotfiles/folder/something.txt",
+        "tests/example-dotfiles/tag-um",
+        "tests/example-dotfiles/tag-um/file.txt",
+        "tests/example-dotfiles/tag-um/.file.txt"
+      ]
+    );
+  }
+}
