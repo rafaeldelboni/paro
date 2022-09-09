11b32f0c43485918d25ef69cd27eba14a2abbd08
Rafael Delboni <rafadelboni@gmail.com>
2022-09-09 09:46:56 -0300

    fix: Better install script error msg

--- install.sh
+++ install.sh
@@ -1,9 +1,63 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
 BIN=paro
 REPO='rafaeldelboni/paro'
 INSTALL_DIR="/usr/local/bin"
 DOWNLOAD_DIR=''
 VERSION=''
 
+# Args
+print_help() {
+    echo "Installs latest (or specific) version of paro. Installation directory defaults to /usr/local/bin."
+    echo -e
+    echo "Usage:"
+    echo "install [--dir <dir>] [--download-dir <download-dir>] [--version <version>]"
+    echo -e
+    echo "Defaults:"
+    echo " * Installation directory: ${INSTALL_DIR}"
+    echo " * Download directory: temporary"
+    echo " * Version: <Latest release on github>"
+    exit 1
+}
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case "$key" in
+        --dir)
+            INSTALL_DIR="$2"
+            shift
+            shift
+            ;;
+        --download-dir)
+            DOWNLOAD_DIr="$2"
+            shift
+            shift
+            ;;
+        --version)
+            VERSION="$2"
+            shift
+            shift
+            ;;
+        *)  # unknown option
+            print_help
+            shift
+            ;;
+    esac
+done
+
+if [[ -z "$DOWNLOAD_DIR" ]]; then
+    DOWNLOAD_DIR="$(mktemp -d)"
+    trap 'rm -rf "$DOWNLOAD_DIR"' EXIT
+fi
+
+if [[ "$VERSION" == "" ]]; then
+    VERSION=$(curl -s https://api.github.com/repos/${REPO}/releases/latest | grep -o "\"tag_name\": \".*\"" | cut -d'"' -f4)
+fi
+
+# Local enviroment
 case "$(uname -s)" in
     Linux*)  PLATFORM=unknown-linux-gnu;;
     Darwin*) PLATFORM=apple-darwin;;
@@ -14,15 +68,7 @@ case "$(uname -m)" in
     aarch64*) ARCH=aarch64;;
 esac
 
-if [[ -z "$DOWNLOAD_DIR" ]]; then
-    DOWNLOAD_DIR="$(mktemp -d)"
-    trap 'rm -rf "$DOWNLOAD_DIR"' EXIT
-fi
-
-if [[ "$version" == "" ]]; then
-    VERSION=$(curl -s https://api.github.com/repos/${REPO}/releases/latest | grep -o "\"tag_name\": \".*\"" | cut -d'"' -f4)
-fi
-
+# Build file name and download link
 FILEEXT=zip
 FILENAME=paro-${VERSION}-${ARCH}-${PLATFORM}
 FILE=${FILENAME}.${FILEEXT}
@@ -35,6 +81,7 @@ else
     UTIL="$(which tar) -zxf"
 fi
 
+# Download
 # Running this part in a subshell so when it finishes we go back to the previous directory
 mkdir -p "$DOWNLOAD_DIR" && (
     cd "$DOWNLOAD_DIR"
@@ -45,6 +92,7 @@ mkdir -p "$DOWNLOAD_DIR" && (
     rm -f "$FILE"
 )
 
+# Install
 if [[ "$DOWNLOAD_DIR" != "$INSTALL_DIR" ]]
 then
     mkdir -p "$INSTALL_DIR"
